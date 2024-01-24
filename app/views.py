5f4558804c028eb3154961aeea9ef7e67dea466e
Abi123-suara <abinash998143@gmail.com>
2024-01-24 17:50:59 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from app.forms import *
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.core.mail import send_mail
+from django.contrib.auth import authenticate,login,logout
+from django.urls import reverse
+from app.models import *
+from django.contrib.auth.decorators import login_required
 
 
 def registration(request):
@@ -30,7 +34,7 @@ def registration(request):
 
 
             send_mail('Registration',
-            'Au sangate khaichaaa',
+            'Registered Sucessfully',
             'suaraabinash1@gmail.com',
             [MUFDO.email],
             fail_silently=False)
@@ -41,3 +45,31 @@ def registration(request):
             return HttpResponse("invalid Data")
    
     return render(request,'registration.html',d)
+
+
+def home(request):
+    if request.session.get('username'):
+        username=request.session.get('username')
+        d={'username':username}
+        return render(request,'home.html',d)
+    return render(request,'home.html')
+
+
+def user_login(request):
+    if request.method=='POST':
+        username=request.POST['un']
+        password=request.POST['pw']
+        user=authenticate(username=username,password=password)
+
+        if user and user.is_active:
+            login(request,user)
+            request.session['username']=username
+            return HttpResponseRedirect(reverse('home'))
+        else:
+            return HttpResponse('credentials are not matching')
+    return render(request,'user_login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('home'))
--- project34/urls.py
+++ project34/urls.py
@@ -22,7 +22,10 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('registration/',registration,name='registration')
+    path('home/',home,name='home'),
+    path('registration/',registration,name='registration'),
+    path('user_login/',user_login,name='user_login'),
+    path('user_logout/',user_logout,name='user_logout')
 
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
